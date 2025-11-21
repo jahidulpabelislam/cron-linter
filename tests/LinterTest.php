aabb5900aebdd6cf7f0aa1be4d20cce74456fe39
Jahidul Pabel Islam <jahidul@d3r.com>
2025-11-21 23:58:11 +0000

    Add bunch more tests that are valid expressions

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -9,10 +9,31 @@ use PHPUnit\Framework\TestCase;
 
 final class LinterTest extends TestCase {
 
-    public function testAll(): void {
-        $errors = CronLinter::lintContent(
-            "* * * * * php test.php"
-        );
-        $this->assertEmpty($errors);
+    public function testValid(): void {
+        $errors = CronLinter::lintContent("
+            * * * * * php test.php
+            0 * * * * php test.php
+            10 * * * * php test.php
+            59 * * * * php test.php
+            1,2 * * * * php test.php
+            * 0 * * * php test.php
+            * 2 * * * php test.php
+            * 23 * * * php test.php
+            * 2,1 * * * php test.php
+            * * 1 * * php test.php
+            * * 31 * * php test.php
+            * * 10,11 * * php test.php
+            * * * 1 * php test.php
+            * * * 12 * php test.php
+            * * * 2,9 * php test.php
+            * * * feb * php test.php
+            * * * JUN * php test.php
+            * * * * 0 php test.php
+            * * * * 6 php test.php
+            * * * * 5,4 php test.php
+            * * * * fri php test.php
+            * * * * TuE php test.php
+        ");
+        $this->assertEquals([], $errors);
     }
 }
