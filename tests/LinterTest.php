a3abe4f3b900c7bd645d0722eae28f09f365a469
Jahidul Pabel Islam <jahidul@d3r.com>
2025-11-22 11:41:37 +0000

    Use provider for valid test

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -9,32 +9,39 @@ use PHPUnit\Framework\TestCase;
 
 final class LinterTest extends TestCase {
 
-    public function testValid(): void {
-        $errors = CronLinter::lintContent("
-            * * * * * php test.php
-            0 * * * * php test.php
-            10 * * * * php test.php
-            59 * * * * php test.php
-            1,2 * * * * php test.php
-            * 0 * * * php test.php
-            * 2 * * * php test.php
-            * 23 * * * php test.php
-            * 2,1 * * * php test.php
-            * * 1 * * php test.php
-            * * 31 * * php test.php
-            * * 10,11 * * php test.php
-            * * * 1 * php test.php
-            * * * 12 * php test.php
-            * * * 2,9 * php test.php
-            * * * feb * php test.php
-            * * * JUN * php test.php
-            * * * * 0 php test.php
-            * * * * 6 php test.php
-            * * * * 5,4 php test.php
-            * * * * fri php test.php
-            * * * * TuE php test.php
-        ");
-        $this->assertEquals([], $errors);
+    public function validProvider(): array {
+        return [
+            ["* * * * * php test.php"],
+            ["0 * * * * php test.php"],
+            ["10 * * * * php test.php"],
+            ["1,2 * * * * php test.php"],
+            ["* 0 * * * php test.php"],
+            ["* 2 * * * php test.php"],
+            ["* 23 * * * php test.php"],
+            ["* 2,1 * * * php test.php"],
+            ["* * 1 * * php test.php"],
+            ["* * 31 * * php test.php"],
+            ["* * 10,11 * * php test.php"],
+            ["* * * 1 * php test.php"],
+            ["* * * 12 * php test.php"],
+            ["* * * 2,9 * php test.php"],
+            ["* * * feb * php test.php"],
+            ["* * * JUN * php test.php"],
+            ["* * * * 0 php test.php"],
+            ["* * * * 6 php test.php"],
+            ["* * * * 5,4 php test.php"],
+            ["* * * * fri php test.php"],
+            ["* * * * TuE php test.php"],
+        ];
+    }
+
+    /**
+     * @dataProvider validProvider
+     * @return void
+     */
+    public function testValid(string $expression): void {
+        $errors = CronLinter::lintContent($expression);
+        $this->assertCount(0, $errors);
     }
 
     public function invalidProvider(): array {
