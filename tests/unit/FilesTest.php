05bd8135cf003451fb581f4f3008981f214c1f87
Jahidul Pabel Islam <jahidul@d3r.com>
2026-07-13 23:51:04 +0100

    Check number of files checked too

--- tests/unit/FilesTest.php
+++ tests/unit/FilesTest.php
@@ -16,28 +16,34 @@ final class FilesTest extends TestCase {
     public function testWithGlob(): void {
         $linter = CronLinter::lintFiles([__DIR__ . "/../fixtures/valid/cron.*"]);
         $this->assertCount(0, $linter->getErrors());
+        $this->assertSame(2, $linter->getNumberOfFilesChecked());
     }
 
     public function testWithDirectoryGlob(): void {
         $linter = CronLinter::lintFiles([__DIR__ . "/../fixtures/valid/cron.d/*"]);
         $this->assertCount(0, $linter->getErrors());
+        $this->assertSame(2, $linter->getNumberOfFilesChecked());
     }
 
     public function testWithGlobAndBaseDir(): void {
         $linter = CronLinter::lintFiles(["/cron.*"], __DIR__ . "/../fixtures/valid");
         $this->assertCount(0, $linter->getErrors());
+        $this->assertSame(2, $linter->getNumberOfFilesChecked());
     }
 
     public function testWithGlobNoMatches(): void {
         $linter = CronLinter::lintFiles([__DIR__ . "/../fixtures/valid/nonexistent.*"]);
         $this->assertCount(0, $linter->getErrors());
+        $this->assertSame(0, $linter->getNumberOfFilesChecked());
     }
 
     public function testWithGlobAndInvalidContent(): void {
-        $errors = CronLinter::lintFiles([__DIR__ . "/../fixtures/invalid/cron.*"])->getErrors();
+        $linter = CronLinter::lintFiles([__DIR__ . "/../fixtures/invalid/cron.*"]);
+        $errors = $linter->getErrors();
         $this->assertCount(1, $errors);
-        $expectedFile = __DIR__ . "/../fixtures/invalid/cron.daily";
-        $this->assertArrayHasKey($expectedFile, $errors);
-        $this->assertSame(["Line 1 has missing time expression"], $errors[$expectedFile]);
+        $filepath = __DIR__ . "/../fixtures/invalid/cron.daily";
+        $this->assertArrayHasKey($filepath, $errors);
+        $this->assertSame(["Line 1 has missing time expression"], $errors[$filepath]);
+        $this->assertSame(1, $linter->getNumberOfFilesChecked());
     }
 }
